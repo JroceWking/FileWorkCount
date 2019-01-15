376ca0d6f6bef44f6a2e2949d7f880d71d391659
Jroe <m15599104105@163.com>
2019-01-15 04:24:17 -0500

    Initial commit

---
+++ src/main/scala/com/spark/FileWork/FileWordCount.scala
@@ -0,0 +1,18 @@
+package com.spark.FileWork
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object FileWordCount {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("FileWordCount")
+    val ssc = new StreamingContext(sparkConf,Seconds(5))
+    val lines = ssc.textFileStream("file:///opt/NetworkWordCount/log")
+    val result = lines.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+result.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
